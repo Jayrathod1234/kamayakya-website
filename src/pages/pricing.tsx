bd27ec9a71cbd6787a23805dd471cd481e41fe1b
Sahil lakhani <sahillakhani266@gmail.com>
2024-07-12 18:03:08 +0530

    margin fix in the pricing page

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -162,7 +162,7 @@ export default function Page() {
           </div>
 
           <div className=" mt-[1.875rem] md:mt-9 text-center">
-            <h1 className=" text-display-xs md:text-display-lg font-bold mt-20">
+            <h1 className=" text-display-xs md:text-display-lg font-bold">
               What type of <span className=" text-brand-400">investor</span> are you?
             </h1>
             <p className=" text-md md:text-lg mt-3 text-gray-600 mb-9">
