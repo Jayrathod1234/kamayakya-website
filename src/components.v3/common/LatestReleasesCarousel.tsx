d4a8b8f1371826f278123a8fa650d58457308bb2
Darshan Tarpada <108277109+micradarshan@users.noreply.github.com>
2024-08-07 11:42:41 +0530

    Update LatestReleasesCarousel.tsx

--- src/components.v3/common/LatestReleasesCarousel.tsx
+++ src/components.v3/common/LatestReleasesCarousel.tsx
@@ -11,7 +11,6 @@ import ClassNames from "embla-carousel-class-names";
 import LatestReleasesCard from "./LatestReleasesCard"
 
 const carouselItem = [
-
   <LatestReleasesCard percentage="12.24" />,
   <LatestReleasesCard percentage="20.24" />,
   <LatestReleasesCard percentage="20.24" />,
