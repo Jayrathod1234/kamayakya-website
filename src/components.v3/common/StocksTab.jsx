022c1f8b28699e2a668cb5b8439be5da78c11b84
Neha-micra <neha.k.micra@gmail.com>
2024-08-08 18:14:28 +0530

    Become-Button
    Become member 30% completed

--- src/components.v3/common/StocksTab.jsx
+++ src/components.v3/common/StocksTab.jsx
@@ -1,69 +1,9 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
+import React from "react";
 
-function StocksTab(props) {
-  const { children, value, index, ...other } = props;
-
+function StocksTab() {
   return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  );
-}
-
-StocksTab.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-
-export default function BasicTabs() {
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <>
-      <Box sx={{ width: "100%" }}>
-        <Box sx={{ borderBottom: 0, borderColor: "divider" }}>
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            aria-label="basic tabs example"
-          >
-            <Tab label="Main Board" {...a11yProps(0)} />
-            <Tab label="Item Two" {...a11yProps(1)} />
-          </Tabs>
-        </Box>
-        {/* <StocksTab value={value} index={0}>
-        Item One
-      </StocksTab>
-      <StocksTab value={value} index={1}>
-        Item Two
-      </StocksTab>
-      <StocksTab value={value} index={2}>
-        Item Three
-      </StocksTab> */}
-      </Box>
-      {/* <div className="radio-input h-[68px] w-[333px] relative flex items-center rounded-[61px] p-1.5 bg-white text-black overflow-hidden border-[6px] border-[#ffffff3a]">
+    <div>
+      <div className="radio-input h-[68px] w-[333px] relative flex items-center rounded-[61px] p-1.5 bg-white text-black overflow-hidden border-[6px] border-[#ffffff3a]">
         <label className="w-full p-[10px] cursor-pointer justify-center items-center z-[1] font-semibold text-sm">
           <input type="radio" id="value-1" name="value-radio" value="value-1" />
           <span className="text-md font-semibold font-open_sans">
@@ -72,7 +12,7 @@ export default function BasicTabs() {
           </span>
           <div>
             {" "}
-            <span className="text-3xs font-bold text-[#D0D5DD]">12 Stocks</span>
+            <span className="text-3xs font-bold">12 Stocks</span>
           </div>
         </label>
         <label>
@@ -81,15 +21,17 @@ export default function BasicTabs() {
             SME Board
           </span>
           <div>
-            <span className="text-3xs font-bold text-[#667085]">14 Stocks</span>
+            <span className="text-3xs font-bold ">14 Stocks</span>
           </div>
         </label>
-        <label>
+        {/* <label>
           <input type="radio" id="value-3" name="value-radio" value="value-3" />
           <span>Option 3</span>
-        </label>
+        </label> */}
         <span className="selection hidden absolute h-full w-[166px] left-0 top-0 px-10 py-2 rounded-[47px] "></span>
-      </div> */}
-    </>
+      </div>
+    </div>
   );
 }
+
+export default StocksTab;
