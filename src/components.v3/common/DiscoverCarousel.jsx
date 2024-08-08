aa8f27925326058b692ce7a8ae6dfee4ad5be90d
Neha-micra <neha.k.micra@gmail.com>
2024-08-08 16:26:02 +0530

    discover card
    icon set

--- src/components.v3/common/DiscoverCarousel.jsx
+++ src/components.v3/common/DiscoverCarousel.jsx
@@ -4,39 +4,6 @@ import Discovercard from "./Discovercard";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { Button } from "../../components.v2/ui/button";
 
-const useSelectedSnapDisplay = (emblaApi) => {
-  const [selectedSnap, setSelectedSnap] = useState(0);
-  const [snapCount, setSnapCount] = useState(0);
-
-  const updateScrollSnapState = useCallback((emblaApi) => {
-    setSnapCount(emblaApi.scrollSnapList().length);
-    setSelectedSnap(emblaApi.selectedScrollSnap());
-  }, []);
-
-  useEffect(() => {
-    if (!emblaApi) return;
-
-    updateScrollSnapState(emblaApi);
-    emblaApi.on("select", updateScrollSnapState);
-    emblaApi.on("reInit", updateScrollSnapState);
-  }, [emblaApi, updateScrollSnapState]);
-
-  return {
-    selectedSnap,
-    snapCount,
-  };
-};
-
-const SelectedSnapDisplay = (props) => {
-  const { selectedSnap, snapCount } = props;
-
-  return (
-    <div className="embla__selected-snap-display">
-      {selectedSnap + 1} / {snapCount}
-    </div>
-  );
-};
-
 const usePrevNextButtons = (emblaApi) => {
   const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
   const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
@@ -126,7 +93,6 @@ const DiscoverCarousel = (props) => {
     onNextButtonClick,
   } = usePrevNextButtons(emblaApi);
 
-  const { selectedSnap, snapCount } = useSelectedSnapDisplay(emblaApi);
   const carouselItems = [
     {
       name: "Value Pick",
@@ -228,11 +194,6 @@ const DiscoverCarousel = (props) => {
           <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
           <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
         </div>
-
-        <SelectedSnapDisplay
-          selectedSnap={selectedSnap}
-          snapCount={snapCount}
-        />
       </div>
     </>
   );
