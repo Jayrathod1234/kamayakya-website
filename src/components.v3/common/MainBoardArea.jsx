442e7d9cfda07ad9a90d1d31b55d2c2094575ab9
Darshan <darshan.micrasol@gmail.com>
2024-08-12 17:48:48 +0530

    Update MainBoardArea.jsx

--- src/components.v3/common/MainBoardArea.jsx
+++ src/components.v3/common/MainBoardArea.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
-import Nonlogincard from "./Nonlogincard";
 import MainBoardcard from "./MainBoardcard";
 import Hotstockcard from "./Hotstockcard";
-import HotNewcard from "./HotNewcard";
 import HotNewcard from "./HotNewcard";
 import HotNewCardBlur from "./HotNewCardBlur"
 
