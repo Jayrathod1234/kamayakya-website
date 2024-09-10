66324966311b9c6487c96585ff96ee10531818e7
Neha <neha.k.micra@gmail.com>
2024-09-10 10:24:47 +0530

    Search ,Radio button

--- src/components.v3/common/SearchPage.jsx
+++ src/components.v3/common/SearchPage.jsx
@@ -37,59 +37,62 @@ function SearchPage() {
   const items = response?.pages?.flatMap((page) => page.data) ?? [];
   return (
     <>
-      <div class=" px-[16px]  ">
+      <div class=" bg-[#F2F4F7]  ">
         {/* <!-- Stock Search --> */}
-        <div class="flex items-center bg-white sticky top-0 shadow-lg my-[16px]  rounded-[6px]">
-          {/* <!-- Left icon --> */}
-          <span
-            class="pl-[14px] text-green-800 "
-            onClick={() => setSearchPageOpen(false)}
-          >
-            {/* <!-- Use any icon, like FontAwesome or HeroIcons (example: HeroIcons) --> */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
+        <div className="bg-[#F2F4F7] p-4 sticky top-0 right-0 left-0">
+          <div class="flex items-center bg-white   rounded-[6px] shadow-2xs">
+            {/* <!-- Left icon --> */}
+            <span
+              class="pl-[14px] text-green-800 "
+              onClick={() => setSearchPageOpen(false)}
             >
-              <path
-                d="M15 6L9 12L15 18"
-                stroke="#125B54"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </span>
+              {/* <!-- Use any icon, like FontAwesome or HeroIcons (example: HeroIcons) --> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+              >
+                <path
+                  d="M15 6L9 12L15 18"
+                  stroke="#125B54"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </span>
 
-          {/* <!-- Input field --> */}
-          <input
-            type="search"
-            placeholder="Search Stocks by Name..."
-            class="w-full py-2.5 pl-2 text-gray-500 focus:outline-none rounded-r-lg bg-white"
-            value={searchStock}
-            onChange={(e) => setSearchStock(e.target.value)}
-          />
-          {/* close icon in serach bar  */}
-          {/* <div className="pr-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 16 16"
-              fill="none"
-            >
-              <path
-                d="M12 4L4 12M4 4L12 12"
-                stroke="#667085"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </div> */}
+            {/* <!-- Input field --> */}
+            <input
+              type="search"
+              placeholder="Search Stocks by Name..."
+              class="w-full py-2.5 pl-2 text-gray-500 focus:outline-none rounded-r-lg bg-white"
+              value={searchStock}
+              onChange={(e) => setSearchStock(e.target.value)}
+            />
+            {/* close icon in serach bar  */}
+            <div className="pr-3">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 16 16"
+                fill="none"
+              >
+                <path
+                  d="M12 4L4 12M4 4L12 12"
+                  stroke="#667085"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+          </div>
         </div>
+
         {/* <div class="flex items-center justify-center space-x-2 my-6"> */}
         {/* <!-- Left line --> */}
         {/* <div class="flex-grow border-t border-[#D0D5DD]"></div> */}
@@ -111,26 +114,26 @@ function SearchPage() {
             const href = !isLoggedIn
               ? "#"
               : value.is_blur
-                ? `/pricing`
-                : `/stock-picks/${value.id}`;
+              ? `/pricing`
+              : `/stock-picks/${value.id}`;
             const onClick = !isLoggedIn ? handleLogin : undefined;
             return (
               <Link key={index} href={href}>
                 <div
-                  class="grid grid-cols-1  "
+                  class="grid grid-cols-1  pr-[15px] pl-[17px]"
                   onClick={onClick}
                 >
                   <div className=" shadow  !rounded-lg">
-                    <div class="bg-white mt-1   p-3  items-center space-x-4">
+                    <div class="bg-white    p-3  items-center space-x-4 border-b-2 border-[#F2F4F7]">
                       {stockSector && value.sector && (
-                        <div className="py-[2px] pr-[16px] pl-[6px] rounded-2xl border border-[#FEF0C7] bg-orange-100 flex gap-[4px]  flex-shrink-0 max-w-[142px]">
+                        <div className="py-[2px] pr-[16px] pl-[6px] rounded-2xl border border-[#FEF0C7] bg-orange-100 flex gap-[4px]  flex-shrink-0 max-w-[105px]">
                           <img
                             src="/assets/streamline_hotel-air-conditioner-solid.svg"
                             alt=""
                             className="w-3"
                           />
 
-                          <p className="text-[10px] font-semibold text-[#A3651A] text-nowrap font-open_sans">
+                          <p className="text-[10px] font-semibold text-[#A3651A] text-nowrap font-open_sans truncate">
                             {stockSector[value.sector]}
                           </p>
                         </div>
@@ -149,7 +152,7 @@ function SearchPage() {
                         </div>
                         <div className="ml-[10px]">
                           {value.is_blur ? (
-                            <div className="h-5 bg-[#EDF0F5] rounded-[20px] min-w-[200px] sm:min-w-[200px] md:min-w-[200px] slg:min-w-[200px] lg:min-w-[200px]"></div>
+                            <div className="h-5 bg-[#EDF0F5] rounded-[20px] min-w-[200px] sm:min-w-[206px] md:min-w-[200px] slg:min-w-[208px] lg:min-w-[271px]"></div>
                           ) : (
                             <h2 class="text-md font-medium text-[#0C111D] font-open_sans mb-0">
                               {" "}
@@ -167,10 +170,11 @@ function SearchPage() {
                             <div class="text-[#98A2B3] font-medium font-open_sans flex gap-2 items-center">
                               Total Returns:
                               <span
-                                class={`${value.gain_loss >= 0
-                                  ? "text-green-600"
-                                  : "text-red-600"
-                                  } font-bold text-sm`}
+                                class={`${
+                                  value.gain_loss >= 0
+                                    ? "text-green-600"
+                                    : "text-red-600"
+                                } font-bold text-sm`}
                               >
                                 {value.gain_loss == null ? (
                                   <>-</>
