6f598e9b347a283720708e1d7c6b38fa770b074c
sahilpadvi <sahilpadvi06@gmail.com>
2024-11-13 11:56:45 +0530

    feat: signup modal

--- src/components.v3/common/LoginPrompt.tsx
+++ src/components.v3/common/LoginPrompt.tsx
@@ -1,5 +1,12 @@
 import { Dialog, DialogContent, DialogTrigger } from "@/components.v2/ui/dialog";
 import React from "react";
+import PhoneInput, { isPossiblePhoneNumber, isValidPhoneNumber } from "react-phone-number-input";
+import "react-phone-number-input/style.css";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { Button } from "@/components.v2/button";
+import { ButtonVariant } from "@/components.v2/button/button";
+import { Line } from "@/components.v2/blogs/blog-card-sm";
+import { Mail } from "lucide-react";
 
 interface ILoginPrompt {
   triggerEle: React.ReactNode;
@@ -15,11 +22,22 @@ const NewUserList = ({ label }: { label: string }) => {
 };
 
 export default function LoginPrompt({ triggerEle }: ILoginPrompt) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    setValue,
+  } = useForm({
+    defaultValues: {
+      phone: "",
+    },
+  });
   return (
     <Dialog>
       <DialogTrigger>{triggerEle}</DialogTrigger>
       <DialogContent closeClassName=" hidden" className=" flex !p-0 overflow-hidden open_sans  min-w-fit max-w-fit">
-        <div className=" py-10 pb-4 bg-[#FFECDB] ">
+        <div className=" py-10 pb-4 bg-[#FFECDB] max-w-[352px] ">
           <div className=" flex flex-col items-center">
             <img width={26} height={32} src="/KKLogoK.svg" alt="kklogo" />
             <div className=" p-4  rounded-lg border border-[#FFFFFF] bg-[#FFFFFF66] mt-[14px] flex flex-col gap-y-3">
@@ -34,7 +52,56 @@ export default function LoginPrompt({ triggerEle }: ILoginPrompt) {
         </div>
         <div className=" p-[60px]">
           <h2 className=" text-gray-900 text-display-xs font-bold">Sign in to KamayaKya</h2>
-          
+          <div className=" mt-9">
+            <p className=" font-medium mb-[6px] text-2xs">Mobile Number</p>
+            <div className=" border border-[#0000000F] py-2 px-[10px] rounded-lg">
+              <Controller
+                name="phone"
+                control={control}
+                rules={{
+                  required: "Enter phone to continue",
+                  validate: (value) => {
+                    return isPossiblePhoneNumber(value) && value.slice(3).length === 10
+                      ? true
+                      : "Enter valid mobile number";
+                  },
+                  // pattern: {
+                  //   value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                  //   message: '"Enter a valid email"',
+                  // },
+                }}
+                render={({ field: { value, onChange } }) => (
+                  <>
+                    {" "}
+                    <PhoneInput
+                      value={value}
+                      onChange={onChange}
+                      defaultCountry="IN"
+                      placeholder="Enter phone number"
+                      className=" border-green-400"
+                    />
+                  </>
+                )}
+              />
+            </div>
+          </div>
+          <div className=" mt-8">
+            <p className=" text-gray-400 text-2xs">
+              By signing in you agree to all our <span className=" text-brand-500 underline">terms & conditions</span>
+            </p>
+            <Button className=" my-[18px] min-w-full max-w-full" variant={ButtonVariant.primary}>
+              <p className=" text-sm font-medium">Request OTP</p>
+            </Button>
+            <div className=" flex items-center mb-[18px]">
+              <div className=" bg-gray-300 h-[1px] w-full"></div>
+              <p className=" text-2xs text-gray-500 mx-3">Or</p>
+              <div className=" bg-gray-300 h-[1px] w-full"></div>
+            </div>
+            <Button className=" max-w-full min-w-full" variant={ButtonVariant.tertiary}>
+              <Mail height={24} width={24}/>
+              <p className=" ml-[10px] text-[#242424] text-sm font-medium">Sign in with Email</p>
+            </Button>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
