ff961076231d719bf1287afac591a807a2eee462
sahilpadvi <sahilpadvi06@gmail.com>
2025-02-13 12:59:50 +0530

    feat: enable 1year plan

--- src/components.v2/payments/plans-section.tsx
+++ src/components.v2/payments/plans-section.tsx
@@ -37,7 +37,7 @@ export function PlansSection() {
   const router = useRouter();
   const tabOptions = [
     { label: "3 Months", value: "3months" },
-    // { label: "1 Year", value: "1year" },
+    { label: "1 Year", value: "1year" },
     // { label: "3 Years", value: "3year" },
   ];
 
@@ -168,7 +168,7 @@ export function PlansSection() {
           <Tabs
             activeValue={currentTab}
             setSelectedOption={setCurrentTab as Dispatch<SetStateAction<string>>}
-            defaultOption="1year"
+            defaultOption={currentTab}
             options={tabOptions}
             variant={TabsVariant.md}
           />
