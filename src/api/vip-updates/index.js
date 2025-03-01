1edb97cdd37c9dd19eeefe6037ff7a129616d6bb
sahilpadvi <sahilpadvi06@gmail.com>
2025-03-01 22:28:47 +0530

    feat: add login skeleton vip updates

--- src/api/vip-updates/index.js
+++ src/api/vip-updates/index.js
@@ -35,66 +35,7 @@ export const getQuarterlyUpdates = async (pagination, isLoggedIn,plan) => {
               created: "2025-02-17T08:48:16.181955Z",
             },
           ],
-          2021: [
-            {
-              id: "55ada006-c9b4-415d-ba4a-f6f5f2bc2d76",
-              year: 2021,
-              quarter: "Q1",
-              pdf: "test_pdf",
-              video_link: "youtube.com",
-              created: "2025-02-07T08:59:34.692213Z",
-            },
-            {
-              id: "4e62fcb9-05ce-428b-878c-628302a64d09",
-              year: 2021,
-              quarter: "Q2",
-              pdf: "test_pdf",
-              video_link: "youtube.com",
-              created: "2025-02-07T08:59:39.391308Z",
-            },
-            {
-              id: "17173792-22f9-41eb-be30-bb478d992a47",
-              year: 2021,
-              quarter: "Q3",
-              pdf: "test_pdf",
-              video_link: "youtube.com",
-              created: "2025-02-07T09:01:27.135739Z",
-            },
-            {
-              id: "a28b42f3-d16e-481d-9914-fe0cdeae13ea",
-              year: 2021,
-              quarter: "Q4",
-              pdf: "test_pdf",
-              video_link: "youtube.com",
-              created: "2025-02-07T09:01:31.013483Z",
-            },
-          ],
-          2020: [
-            {
-              id: "c9e61953-5b84-486a-b8db-a4010c4ff01e",
-              year: 2020,
-              quarter: "Q2",
-              pdf: "test_pdf",
-              video_link: "youtube.com",
-              created: "2025-02-07T05:25:14.540079Z",
-            },
-            {
-              id: "19abac22-684a-40ae-a541-a8588658d23e",
-              year: 2020,
-              quarter: "Q3",
-              pdf: "test_pdf",
-              video_link: "youtube.com",
-              created: "2025-02-07T05:39:43.856536Z",
-            },
-            {
-              id: "78838b38-740b-46fb-99f3-0b923a046ddd",
-              year: 2020,
-              quarter: "Q4",
-              pdf: "test_pdf",
-              video_link: "google2.com",
-              created: "2025-02-07T05:39:49.935108Z",
-            },
-          ],
+          
         },
       };
     }
--- src/pages/vip-updates/index.tsx
+++ src/pages/vip-updates/index.tsx
@@ -2,12 +2,9 @@ import React, { useContext, useEffect, useState } from "react";
 import { Navbar } from "@/components.v2/navbar";
 import { getQuarterlyUpdates } from "../../api/vip-updates/index";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import PDF_ICON from "../../../public/assets/pdf.svg";
-import VIDEO_ICON from "../../../public/assets/play.svg";
 import { Footer } from "@/components.v2/footer";
 import { useQuery } from "@tanstack/react-query";
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components.v2/ui/carousel";
-import { useMediaQuery } from "@mui/material";
+import { Skeleton, useMediaQuery } from "@mui/material";
 import AuthContext from "@/components/AuthContext";
 import { Button, ButtonVariant } from "@/components.v2/button/button";
 import { Button as SButton } from "@/components.v2/ui/button";
@@ -42,7 +39,6 @@ export default function Page() {
       return formattedData;
     },
   });
-  // const [quarterlyUpdates, setQuarterlyUpdates] = useState([]);
   const isMobile = useMediaQuery("(max-width:640px)");
   const isTab = useMediaQuery("(max-width:1024px)");
   const [years, setYears] = useState([]);
@@ -75,145 +71,163 @@ export default function Page() {
       <Navbar />
       <section className="mt-8 sm:mt-[55px] main-container relative z-10">
         <h2 className="text-display-xs sm:text-display-md font-bold text-center">Quarterly Updates</h2>
-
-        <div
-          className={`mt-12 grid grid-cols-[.3fr_1fr_.4fr]  ${
-            years.length === 1
-              ? "lg:grid-cols-[.3fr_1fr_.4fr] sm:grid-cols-[.3fr_1fr_.4fr]"
-              : years.length === 2
-              ? "lg:grid-cols-[.3fr_1fr_1fr_.4fr] sm:grid-cols-[.3fr_1fr_1fr_.4fr] "
-              : " sm:grid-cols-[.3fr_1fr_1fr_.4fr] lg:grid-cols-[.3fr_1fr_1fr_1fr_.4fr]"
-          } items-center place-content-center bg-white rounded-[20px] border border-gray-200 relative`}
-        >
-          {isLoggedIn ? null : (
-            <div className="absolute h-full w-full backdrop-blur-sm flex items-center justify-center">
-              <div className=" flex flex-col justify-center items-center">
-                <div className=" p-[10px] h-fit w-fit  bg-[rgba(255,255,255,1)] rounded-[10px] border border-brand-300 flex items-center justify-center shadow-[0px_0px_40px_-9px_rgba(19,135,137,0.46),0px_4px_40px_12px_rgba(118,237,223,0.05)]">
-                  <img
-                    height={36}
-                    width={36}
-                    className=" object-contain h-9 w-9"
-                    src="/assets/noto_locked.png"
-                    alt="lock"
-                  />
-                </div>
-                <p className=" font-medium mt-[10px] mb-[18px]">Login to Unlock</p>
-                <Button onClick={handleLogin} variant={ButtonVariant.primary}>
-                  Login
-                </Button>
-              </div>
+        {isLoading ? (
+          <div>
+            <div className=" flex items-center justify-center flex-wrap gap-y-1 max-sm:py-3 max-sm:pb-7 sm:gap-6">
+              <Skeleton variant="rounded"  width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} />
+              <Skeleton variant="rounded" width={"25%"} height={45} /> 
             </div>
-          )}
-          {activePlan.plan !== "vip" && isLoggedIn ? (
-            <div className="absolute h-full w-full backdrop-blur-sm flex items-center justify-center">
-              <div className=" flex flex-col justify-center items-center">
-                <div className=" p-[4px] h-fit w-fit  bg-[rgba(255,255,255,1)] rounded-[10px] border border-brand-300 flex items-center justify-center shadow-[0px_0px_40px_-9px_rgba(19,135,137,0.46),0px_4px_40px_12px_rgba(118,237,223,0.05)]">
-                  <div className="bg-[#EBFBF0] p-[7px] rounded-md">
+          </div>
+        ) : (
+          <div
+            className={`mt-12 grid grid-cols-[.3fr_1fr_.4fr]  ${
+              years.length === 1
+                ? "lg:grid-cols-[.3fr_1fr_.4fr] sm:grid-cols-[.3fr_1fr_.4fr]"
+                : years.length === 2
+                ? "lg:grid-cols-[.3fr_1fr_1fr_.4fr] sm:grid-cols-[.3fr_1fr_1fr_.4fr] "
+                : " sm:grid-cols-[.3fr_1fr_1fr_.4fr] lg:grid-cols-[.3fr_1fr_1fr_1fr_.4fr]"
+            } items-center place-content-center bg-white rounded-[20px] border border-gray-200 relative`}
+          >
+            {isLoggedIn ? null : (
+              <div className="absolute h-full w-full backdrop-blur-sm flex items-center justify-center">
+                <div className=" flex flex-col justify-center items-center">
+                  <div className=" p-[10px] h-fit w-fit  bg-[rgba(255,255,255,1)] rounded-[10px] border border-brand-300 flex items-center justify-center shadow-[0px_0px_40px_-9px_rgba(19,135,137,0.46),0px_4px_40px_12px_rgba(118,237,223,0.05)]">
                     <img
-                      height={33}
-                      width={33}
-                      className=" object-contain h-[33px] w-[33px]"
-                      src="/assets/vip_icon.svg"
+                      height={36}
+                      width={36}
+                      className=" object-contain h-9 w-9"
+                      src="/assets/noto_locked.png"
                       alt="lock"
                     />
                   </div>
+                  <p className=" font-medium mt-[10px] mb-[18px]">Login to Unlock</p>
+                  <Button onClick={handleLogin} variant={ButtonVariant.primary}>
+                    Login
+                  </Button>
                 </div>
-                <p className=" font-medium mt-[10px] mb-[18px] text-center">
-                  Text to nudge the user to upgrade to VIP or highlight the benefits of quarterly updates
-                </p>
-                <Button variant={ButtonVariant.primary}>Upgrade to VIP</Button>
               </div>
-            </div>
-          ) : null}
-          {/* Empty cell for alignment */}
-          <div className="py-3 border-b border-b-gray-200 w-full text-md">&nbsp;</div>
-
-          {/* Year headers */}
-          {years.map((year) => (
-            <div key={year} className="text-center font-bold text-md py-3  border-b border-b-gray-200 w-full">
-              FY{year}
-            </div>
-          ))}
-
-          {/* Navigation buttons */}
-          <div className="flex items-center justify-center space-x-5 h-full border-b border-b-gray-200 w-full">
-            {hasPrevious ? (
-              <SButton onClick={previousPage} className=" h-7 w-7 bg-[#0C111D] rounded-full" size="icon">
-                <ChevronLeft size={18} />
-              </SButton>
-            ) : null}
-            {hasNext ? (
-              <SButton onClick={nextPage} className=" h-7 w-7 bg-[#0C111D] rounded-full" size="icon">
-                <ChevronRight size={18} />
-              </SButton>
-            ) : null}
-          </div>
-
-          {/* Quarterly Data */}
-          {QUARTERS.map((quarter) => (
-            <React.Fragment key={quarter}>
-              <div
-                className={`font-semibold py-9 ${
-                  quarter !== "Q1" ? " border-b border-b-gray-200 " : " "
-                } w-full h-full text-center flex items-center justify-center`}
-              >
-                {quarter}
-              </div>
-
-              {years.map((year) => {
-                const quarterYearData = quarterlyUpdates.find((item) => item[`${quarter}_${year}`])?.[
-                  `${quarter}_${year}`
-                ];
-
-                return (
-                  <div
-                    className={` py-9 ${
-                      quarter !== "Q1" ? " border-b border-b-gray-200 " : " "
-                    } w-full h-full flex items-center justify-center flex-wrap gap-4`}
-                    key={year}
-                  >
-                    {quarterYearData && quarterYearData.length > 0 ? (
-                      quarterYearData.map((entry, index) => (
-                        <>
-                          <a
-                            key={index}
-                            href={entry.pdf}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="py-[10px] pl-[10px] pr-[6px] bg-white border border-gray-200 rounded-lg shadow-md flex items-center gap-x-2 text-inherit text-md text-gray-950"
-                          >
-                            <img height={20} width={20} src={"/assets/pdf.svg"} className=" object-cover" />
-                            <span>Presentation</span>
-                            <ChevronRight color="#D0D5DD" size={16} />
-                          </a>
-                          <a
-                            key={index}
-                            href={entry.video_link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="py-[10px] pl-[10px] pr-[6px] bg-white border border-gray-200 rounded-lg shadow-md flex items-center gap-x-2 text-inherit text-md text-gray-950"
-                          >
-                            <img height={20} width={20} src={"/assets/play.svg"} className=" object-cover" />
-                            <span>Video</span>
-                            <ChevronRight color="#D0D5DD" size={16} />
-                          </a>
-                        </>
-                      ))
-                    ) : (
-                      <div className="text-gray-500">NA</div>
-                    )}
+            )}
+            {activePlan.plan !== "vip" && isLoggedIn ? (
+              <div className="absolute h-full w-full backdrop-blur-sm flex items-center justify-center">
+                <div className=" flex flex-col justify-center items-center">
+                  <div className=" p-[4px] h-fit w-fit  bg-[rgba(255,255,255,1)] rounded-[10px] border border-brand-300 flex items-center justify-center shadow-[0px_0px_40px_-9px_rgba(19,135,137,0.46),0px_4px_40px_12px_rgba(118,237,223,0.05)]">
+                    <div className="bg-[#EBFBF0] p-[7px] rounded-md">
+                      <img
+                        height={33}
+                        width={33}
+                        className=" object-contain h-[33px] w-[33px]"
+                        src="/assets/vip_icon.svg"
+                        alt="lock"
+                      />
+                    </div>
                   </div>
-                );
-              })}
-              <div className={`py-9 ${quarter !== "Q1" ? " border-b border-b-gray-200 " : " "} w-full  h-full`}>
-                &nbsp;
+                  <p className=" font-medium mt-[10px] mb-[18px] text-center">
+                    Text to nudge the user to upgrade to VIP or highlight the benefits of quarterly updates
+                  </p>
+                  <Button variant={ButtonVariant.primary}>Upgrade to VIP</Button>
+                </div>
               </div>
-            </React.Fragment>
-          ))}
-        </div>
+            ) : null}
+            {/* Empty cell for alignment */}
+            <div className="py-3 border-b border-b-gray-200 w-full text-md">&nbsp;</div>
+
+            {/* Year headers */}
+            {years.map((year) => (
+              <div key={year} className="text-center font-bold text-md py-3  border-b border-b-gray-200 w-full">
+                FY{year}
+              </div>
+            ))}
+
+            {/* Navigation buttons */}
+            <div className="flex items-center justify-center space-x-5 h-full border-b border-b-gray-200 w-full">
+              {hasPrevious ? (
+                <SButton onClick={previousPage} className=" h-7 w-7 bg-[#0C111D] rounded-full" size="icon">
+                  <ChevronLeft size={18} />
+                </SButton>
+              ) : null}
+              {hasNext ? (
+                <SButton onClick={nextPage} className=" h-7 w-7 bg-[#0C111D] rounded-full" size="icon">
+                  <ChevronRight size={18} />
+                </SButton>
+              ) : null}
+            </div>
+
+            {/* Quarterly Data */}
+            {QUARTERS.map((quarter) => (
+              <React.Fragment key={quarter}>
+                <div
+                  className={`font-semibold py-9 ${
+                    quarter !== "Q1" ? " border-b border-b-gray-200 " : " "
+                  } w-full h-full text-center flex items-center justify-center`}
+                >
+                  {quarter}
+                </div>
+
+                {years.map((year) => {
+                  const quarterYearData = quarterlyUpdates.find((item) => item[`${quarter}_${year}`])?.[
+                    `${quarter}_${year}`
+                  ];
+
+                  return (
+                    <div
+                      className={` py-9 ${
+                        quarter !== "Q1" ? " border-b border-b-gray-200 " : " "
+                      } w-full h-full flex items-center justify-center flex-wrap gap-4`}
+                      key={year}
+                    >
+                      {quarterYearData && quarterYearData.length > 0 ? (
+                        quarterYearData.map((entry, index) => (
+                          <>
+                            <a
+                              key={index}
+                              href={entry.pdf}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="py-[10px] pl-[10px] pr-[6px] bg-white border border-gray-200 rounded-lg shadow-md flex items-center gap-x-2 text-inherit text-md text-gray-950"
+                            >
+                              <img height={20} width={20} src={"/assets/pdf.svg"} className=" object-cover" />
+                              <span>Presentation</span>
+                              <ChevronRight color="#D0D5DD" size={16} />
+                            </a>
+                            <a
+                              key={index}
+                              href={entry.video_link}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="py-[10px] pl-[10px] pr-[6px] bg-white border border-gray-200 rounded-lg shadow-md flex items-center gap-x-2 text-inherit text-md text-gray-950"
+                            >
+                              <img height={20} width={20} src={"/assets/play.svg"} className=" object-cover" />
+                              <span>Video</span>
+                              <ChevronRight color="#D0D5DD" size={16} />
+                            </a>
+                          </>
+                        ))
+                      ) : (
+                        <div className="text-gray-500">NA</div>
+                      )}
+                    </div>
+                  );
+                })}
+                <div className={`py-9 ${quarter !== "Q1" ? " border-b border-b-gray-200 " : " "} w-full  h-full`}>
+                  &nbsp;
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
+        )}
       </section>
       <div className=" mt-4">
-      <ElevateSection/>
+        <ElevateSection />
       </div>
       <Footer />
     </main>
