63793785e134903f7f42fe8cfac36c2b321e0ae1
sahilpadvi <sahilpadvi06@gmail.com>
2025-12-24 18:40:50 +0530

    feat:update tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -95,6 +95,35 @@ function MyApp({ Component, pageProps }: AppProps) {
                       {/*/>*/}
                     </Head>
                     <Toaster />
+                    {/* Facebook Pixel */}
+                    <Script
+                      id="facebook-pixel"
+                      strategy="afterInteractive"
+                      dangerouslySetInnerHTML={{
+                        __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1179690674320602');
+            fbq('track', 'PageView');
+          `,
+                      }}
+                    />
+
+                    {/* Noscript fallback */}
+                    <noscript>
+                      <img
+                        height="1"
+                        width="1"
+                        style={{ display: "none" }}
+                        src="https://www.facebook.com/tr?id=1179690674320602&ev=PageView&noscript=1"
+                      />
+                    </noscript>
                     <Script src={`https://www.googletagmanager.com/gtag/js?id=G-4R949GS408`} />
 
                     <Script id="google-analytics">
